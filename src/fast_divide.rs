eb5192a16666619759f08f5794c2bd08f944fd62
Tomek Czajka <tczajka@gmail.com>
2021-03-10 22:20:55 +0100

    Branchless FastDivide

--- src/fast_divide.rs
+++ src/fast_divide.rs
@@ -76,11 +76,18 @@ impl FastDivideSmall {
     }
 }
 
-/// Fast repeated division by a given normalized Word.
+/// Divide a DoubleWord by a prearranged divisor.
+///
+/// Assumes quotient fits in a Word.
+///
+/// Granlund, Montgomerry "Division by Invariant Integers using Multiplication"
+/// Algorithm 8.1.
 #[derive(Clone, Copy)]
 pub(crate) struct FastDivideNormalized {
+    // Top bit must be 1.
     divisor: Word,
-    reciprocal: Word,
+    // floor ((2^2N - 1) / divisor) - 2^N
+    m: Word,
 }
 
 impl FastDivideNormalized {
@@ -88,30 +95,97 @@ impl FastDivideNormalized {
     ///
     /// divisor must have top bit of 1
     pub(crate) const fn new(divisor: Word) -> Self {
-        let (recip_lo, _) = split_double_word(DoubleWord::MAX / extend_word(divisor));
+        // assert!(divisor.leading_zeros() == 0);
+        // Asserts don't work in const functions.
+        let (m, _hi) = split_double_word(DoubleWord::MAX / extend_word(divisor));
+        // assert!(_hi == 1);
 
-        FastDivideNormalized {
-            divisor,
-            reciprocal: recip_lo,
-        }
+        // Note:
+        // m > 0
+        // (m + 2^N) * divisor < 2^2N
+        // (m + 2^N) * divisor >= 2^2N - divisor
+
+        FastDivideNormalized { divisor, m }
     }
 
-    /// Divide a value.
+    /// (n / divisor, n % divisor)
     /// The result must fit in a single word.
-    pub(crate) fn div_rem(&self, dividend: DoubleWord) -> (Word, Word) {
-        let (_, dividend_hi) = split_double_word(dividend);
-        // Approximate quotient: it may be too small by at most 3.
-        // self.reciprocal + (1<<BITS) is approximately (1<<(2*BITS)) / self.divisor.
-        let (_, mul_hi) =
-            split_double_word(extend_word(self.reciprocal) * extend_word(dividend_hi));
-        let mut quotient = mul_hi + dividend_hi;
-        let mut remainder = dividend - extend_word(self.divisor) * extend_word(quotient);
-        while remainder >= extend_word(self.divisor) {
-            quotient += 1;
-            remainder -= extend_word(self.divisor);
-        }
-        let (rem_lo, _) = split_double_word(remainder);
-        (quotient, rem_lo)
+    pub(crate) fn div_rem(&self, n: DoubleWord) -> (Word, Word) {
+        // Let d = divisor.
+        // Let (2^N + m) * d = 2^2N - k, 0 < k <= d
+        // Approximate 2^N * quotient is:
+        // 2^N * n / d <= (2^N + m) * n / 2^N = n + n*m / 2^N
+        //
+        // Let [n_hi, n_lo] = n.
+        // b = top bit of n_lo
+        //
+        // Calculate:
+        // big_q = n + (n_hi + b) * m + b * (d - 2^N)
+        //
+        // big_q >= n + b * (d - 2^N) >= n - b * 2^(N-1) >= 0
+        // big_q = n_hi * (m + 2^N) + n_lo + b * (m - 2^N + divisor)
+        //       <= (d-1) * (m + 2^N) + (2^N-1) + (m - 2^N + 2^N - 1)
+        //       = d * (m + 2^N) - 2 < 2^2N
+        // So big_q fits in DoubleWord.
+        //
+        // Let big_q = (q_hi, q_lo).
+        //
+        // We will use q_hi as the quotient approximation.
+        //
+        // remainder = n - q_hi * d
+        // = (n * 2^N - big_q * d + q_lo * d) / 2^N
+        // = (n * 2^N - (n_hi * (m+2^N) + n_lo + b(m-2^N+d)) * d + q_lo * d) / 2^N
+        // = (n * 2^N - ((n_hi+b) * (m+2^N) + n_lo + b(d - 2^(N+1))) * d + q_lo*d) / 2^N
+        // = (n * 2^N - (n_hi+b) * (m+2^N)d - n_lo * d - b * d * (d - 2^(N+1)) + q_lo * d) / 2^N
+        // = (n_hi * 2^2N + n_lo * 2^N - (n_hi+b) * (2^2N-k) - n_lo * d - b*d*(d-2^(N+1)) + q_lo*d)/2^N
+        // = (n_lo * (2^N-d) + (n_hi+b)*k - b*2^2N - b*d*(d-2^(N+1)) + q_lo*d) / 2^N
+        // = (n_lo * (2^N-d) + (n_hi+b)*k - b*(2^2N + d^2 - d*2^(N+1)) + q_lo*d) / 2^N
+        // = (n_lo * (2^N-d) + (n_hi+b)*k - b*(2^N-d)^2 + q_lo*d) / 2^N
+        // = ((n_hi+b)*k + q_lo*d)/2^N + (1-d*2^-N)(n_lo + b*(d-2^N))
+        // = ((n_hi+b)*k + q_lo*d)/2^N + (1-d*2^-N)((n_lo-b*2^(N-1)) + b * (d-2^(N-1)))
+        //
+        // remainder >= 0
+        //
+        // On the other hand:
+        // remainder <= ((d-1+1)*d + (2^N-1)*d)/2^N + (1-d*2^-N)(2^(N-1)-1 + (d-2^(N-1)))
+        // < d^2/2^N + d + (1-d*2^-N)*d = 2*d
+        //
+        // Therefore the q_hi may only be 1 too small.
+
+        let (n_lo, n_hi) = split_double_word(n);
+        // debug_assert!(n_hi < d);
+        let b = n_lo >> (WORD_BITS - 1);
+        // Spread bit b on all bits.
+        let b_spread = (0 as Word).wrapping_sub(b);
+
+        // big_q = n + (n_hi + b) * m + b * (d - 2^N)
+        // adjustment = n_lo + b * (d - 2^N) >= 0
+        // q = q_hi is the high word of big_q
+        let adjustment = n_lo.wrapping_add(b_spread & self.divisor);
+        let (_, x_hi) = split_double_word(
+            extend_word(n_hi + b) * extend_word(self.m) + extend_word(adjustment),
+        );
+        let q = n_hi + x_hi;
+
+        // q2 = 2^N - (q+1)
+        let q2 = Word::MAX - q;
+
+        // rem = n - (q+1)*d = n - 2^N * d + q2 * d
+        // We are keeping it modulo 2^2N.
+        // If rem >= 0, then the result is (q+1, rem).
+        // If rem < 0, then the result is (q, rem + d).
+        let rem = n
+            .wrapping_sub(double_word(0, self.divisor))
+            .wrapping_add(extend_word(q2) * extend_word(self.divisor));
+
+        let (rem_lo, rem_hi) = split_double_word(rem);
+        // rem_hi is 0 or -1
+        // quotient = q+1 + rem_hi = rem_hi - q2 (mod 2^N)
+        let quotient = rem_hi.wrapping_sub(q2);
+        // remainder = rem + d * (-rem_hi)
+        let remainder = rem_lo.wrapping_add(self.divisor & rem_hi);
+
+        (quotient, remainder)
     }
 }
 
