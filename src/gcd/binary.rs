350f16534bf1fc6fb62b16cd2e4edb46ad3bbf86
Jacob Zhong <cmpute@qq.com>
2022-06-06 22:54:49 -0400

    Fix support for rust 1.49

--- src/gcd/binary.rs
+++ src/gcd/binary.rs
@@ -87,7 +87,10 @@ pub(crate) fn gcd_in_place(lhs: &mut [Word], rhs: &mut [Word]) -> usize {
     // move the result from rhs to low bits of lhs, with shift taken into account
     let shift_words = init_zeros / WORD_BITS_USIZE;
     let mut final_size = result_cur.len() + shift_words;
-    lhs[..shift_words].fill(0);
+    for i in 0..shift_words {
+        // LEGACY: equivalent to lhs[..shift_words].fill(0) after Rust 1.50
+        lhs[i] = 0;
+    }
     lhs[shift_words..final_size].copy_from_slice(result_cur);
     let carry = shift::shl_in_place(
         &mut lhs[shift_words..final_size],
@@ -275,9 +278,10 @@ pub(crate) fn xgcd_in_place(
 
     // reduce s0 and t0 if s0 > V/g - s0
     // the xx_end markers are ignored here because there could be leading zeros
-    let reduce = cmp_same_len(s0, s1).is_ge() || {
+    // LEGACY: use Ordering::is_ge() in Rust 1.53
+    let reduce = !matches!(cmp_same_len(s0, s1), Ordering::Less) || {
         assert!(!add::sub_in_place(s1, s0));
-        let r = cmp_same_len(s0, s1).is_ge();
+        let r = !matches!(cmp_same_len(s0, s1), Ordering::Less);
         assert!(!add::add_in_place(s1, s0));
         r
     };
