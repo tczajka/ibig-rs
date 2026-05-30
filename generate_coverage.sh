d43a414ef5e1ad8631d1ebf43d8b6cf9be1f6a11
Tomek Czajka <tczajka@gmail.com>
2026-05-29 17:46:15 -0700

    Move the ibig crate to the ibig-old subdirectory.

--- generate_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
-export RUSTDOCFLAGS="-Cpanic=abort"
-cargo clean
-cargo +nightly build
-cargo +nightly test
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o coverage
