b4bb1ce13ab96460e5cb10b4a941c76bbb317d24
Tomek Czajka <tczajka@gmail.com>
2026-06-05 18:13:26 -0700

    Add IBig::signum

--- ibig/src/sign.rs
+++ ibig/src/sign.rs
@@ -22,4 +22,32 @@ impl IBig {
             None => !ibig_core::is_negative(self.as_digits()),
         }
     }
+
+    /// Returns a number representing the sign of `self`:
+    /// * `-1` if negative
+    /// * `0` if zero
+    /// * `1` if positive
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ibig::IBig;
+    /// assert_eq!(IBig::from(-5i8).signum(), IBig::from(-1i8));
+    /// assert_eq!(IBig::ZERO.signum(), IBig::ZERO);
+    /// assert_eq!(IBig::from(5i8).signum(), IBig::from(1i8));
+    /// ```
+    #[inline]
+    pub fn signum(&self) -> IBig {
+        match self.try_to_digit() {
+            Some(digit) => IBig::from_digit(digit.signum()),
+            None => {
+                // A multi-digit value is never zero.
+                if ibig_core::is_negative(self.as_digits()) {
+                    IBig::from_i8(-1)
+                } else {
+                    IBig::from_i8(1)
+                }
+            }
+        }
+    }
 }
--- ibig/tests/sign.rs
+++ ibig/tests/sign.rs
@@ -19,3 +19,13 @@ fn is_positive() {
     assert!(IBig::from(u64::MAX).is_positive());
     assert!(!IBig::from(-1i128 << 100).is_positive());
 }
+
+#[test]
+fn signum() {
+    assert_eq!(IBig::ZERO.signum(), IBig::ZERO);
+    assert_eq!(IBig::from(5i8).signum(), IBig::from(1i8));
+    assert_eq!(IBig::from(-5i8).signum(), IBig::from(-1i8));
+    assert_eq!(IBig::from(u64::MAX).signum(), IBig::from(1i8));
+    assert_eq!(IBig::from(1i128 << 100).signum(), IBig::from(1i8));
+    assert_eq!(IBig::from(-1i128 << 100).signum(), IBig::from(-1i8));
+}
